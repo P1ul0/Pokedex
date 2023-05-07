759f783038ad8c8144f440dd5c7a8db36f3933f0
Paulo <paulocesar147852@gmail.com>
2023-05-07 16:06:21 -0300

    ajustes

--- src/components/PokeModal/index.jsx
+++ src/components/PokeModal/index.jsx
@@ -1,64 +1,73 @@
+import { BtnFavorito } from "../BtnFavorito";
 import {
-    DivConteudo,
-    DivConteudoModal,
-    DivModal,
-    DivSubModal,
-    DivTypePokemon,
-    ImgClose,
-    ImgPokemon,
-    SubDivConteudo,
-    Text
+  DivConteudo,
+  DivConteudoModal,
+  DivModal,
+  DivStats,
+  DivSubModal,
+  DivTitle,
+  DivTypePokemon,
+  ImgClose,
+  ImgPokemon,
+  StatusBar,
+  SubDivConteudo,
+  Text,
 } from "./style";
 import ReactModal from "react-modal";
-ReactModal.setAppElement('#root');
-export const PokeModal = ({onRequestClose, Pokemon, Background, TextColor}) => {
-    const {
-        id,
-        nome,
-        img,
-        type,
-        stats
-    } = Pokemon
+ReactModal.setAppElement("#root");
+export const PokeModal = ({
+  onRequestClose,
+  Pokemon,
+  Background,
+  TextColor,
+}) => {
+  const { id, nome, img, type, stats } = Pokemon;
 
-    return (
-        <DivModal key={id}>
-            <DivConteudoModal background={Background}>
-                <DivSubModal textColor={TextColor}>
-                    <Text textColor={TextColor}>Detalhes</Text>
-                    <ImgClose textColor={TextColor}
-                        onClick={onRequestClose}/>
-                </DivSubModal>
-                <DivConteudo>
-                    <SubDivConteudo>
-                        <ImgPokemon src={img}/>
-                        <DivSubModal>
-                            <Text Right="10px">#{id}</Text>
-                            <Text textColor={TextColor}>
-                                {nome} </Text>
-                        </DivSubModal>
+  return (
+    <DivModal key={id}>
+      <DivConteudoModal background={Background}>
+        <DivSubModal textColor={TextColor}>
+          <Text textColor={TextColor}>Detalhes</Text>
+          <ImgClose textColor={TextColor} onClick={onRequestClose} />
+        </DivSubModal>
+        <DivConteudo>
+          <SubDivConteudo>
+            <ImgPokemon src={img} />
+            <DivSubModal>
+              <Text textColor={TextColor} Right="10px">
+                #{id}
+              </Text>
+              <Text textColor={TextColor}>{nome} </Text>
+            </DivSubModal>
 
-                        <DivTypePokemon> {
-                            type.map((e) => {
-                                return <Text Right="20px" Size="20px"
-                                    textColor={TextColor}>
-                                    {e}</Text>
-                        })
-                        }</DivTypePokemon>
-                    </SubDivConteudo>
-                </DivConteudo>
-                <DivConteudo>
-                    <SubDivConteudo >
-                        <h1>Estatistica</h1>
-                    </SubDivConteudo>
-                    <SubDivConteudo >
-                        <h1>Ataques</h1>
-                    </SubDivConteudo>
-                </DivConteudo>
-
-
-            </DivConteudoModal>
-        </DivModal>
-
-
-    )
+            <DivTypePokemon>
+              {" "}
+              {type.map((e) => {
+                return (
+                  <Text Right="20px" Size="20px" textColor={TextColor}>
+                    {e}
+                  </Text>
+                );
+              })}
+            </DivTypePokemon>
+          </SubDivConteudo>
+          <DivStats>
+            <DivTitle>
+            <Text textColor={TextColor} Bottom="10px" >Estatisticas</Text>
+            </DivTitle>
+            {stats.map((e) => {
+              return (
+                <>
+                  <Text textColor={TextColor} Size="20px">
+                    {e.name_stat} = {e.base}%
+                  </Text>
+                  <StatusBar percentual={e.base}></StatusBar>
+                </>
+              );
+            })}
+          </DivStats>
+        </DivConteudo>
+      </DivConteudoModal>
+    </DivModal>
+  );
 };
--- src/components/PokeModal/style.js
+++ src/components/PokeModal/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import {TfiClose} from "react-icons/tfi"
+import { TfiClose } from "react-icons/tfi";
 
-export const DivModal = styled.div `
+export const DivModal = styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -10,78 +10,103 @@ export const DivModal = styled.div `
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 49; 
-  background-color: rgba(0,0,0, .5) ;
+  z-index: 49;
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 
-export const DivConteudoModal = styled.div `
+export const DivConteudoModal = styled.div`
   background-color: white;
   padding: 20px;
   border-radius: 10px;
   width: 70vw;
   height: 70vh;
-  z-index: 50; 
-  background-color: ${
-    ({background}) => background ? background : "white"
-};
+  z-index: 50;
+  background-color: ${({ background }) => (background ? background : "white")};
 `;
-export const DivSubModal = styled.div `
+export const DivSubModal = styled.div`
   display: flex;
-  flex-direction: row ;
+  flex-direction: row;
   justify-content: space-between;
-  align-items:center;
+  align-items: center;
   height: 50px;
-  border-bottom: 3px solid ${
-    ({textColor}) => textColor ? textColor : "none"
-} ;
-`
-export const Text = styled.p `
-  font-size: ${({Size}) => Size ? Size : "30px"};
-  font-family: 'Nunito', sans-serif;
-  font-weight:bold;
+  border-bottom: 3px solid
+    ${({ textColor }) => (textColor ? textColor : "none")};
+`;
+export const Text = styled.p`
+  font-size: ${({ Size }) => (Size ? Size : "30px")};
+  font-family: "Nunito", sans-serif;
+  font-weight: bold;
   text-transform: capitalize;
-  margin-right: ${({Right}) => Right ? Right : "none"};
-  color: ${
-    ({textColor}) => textColor ? textColor : "white"
-};
-`
+  margin-bottom: ${({ Bottom }) => (Bottom ? Bottom : "none")};
+  margin-right: ${({ Right }) => (Right ? Right : "none")};
+  margin-left: ${({ Left }) => (Left ? Left : "none")};
+  color: ${({ textColor }) => (textColor ? textColor : "white")};
+`;
 
 export const ImgClose = styled(TfiClose)`
   cursor: pointer;
   height: 35px;
   width: 35px;
   object: contain;
-  color: ${
-    ({textColor}) => textColor ? textColor : "white"
-};
+  color: ${({ textColor }) => (textColor ? textColor : "white")};
 `;
 
-export const DivConteudo = styled.div `
-  height: 62vh;
-  width: 68vw;
+export const DivConteudo = styled.div`
+  height: 50vh;
+  width: 50vw;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
   align-items: center;
-`
-export const SubDivConteudo = styled.div `
-  width: ${
-    ({Width}) => Width ? Width : "auto"
-};
+`;
+export const SubDivConteudo = styled.div`
+  width: ${({ Width }) => (Width ? Width : "1000px")};
+  height: ${({ Height }) => (Height ? Height : "none")};
   display: flex;
   flex-direction: column;
-  align-items: center ; 
-`
-export const ImgPokemon = styled.img `
-  height: 100%;
-  width: 100%;
-  object: contain;
-`
-
-
-export const DivTypePokemon = styled.div `
-  display: flex;
-  flex-direction: row;
+  justify-content: center;
   align-items: center;
+`;
+export const ImgPokemon = styled.img`
+  width: 80%;
+  object: contain;
+`;
+export const DivTypePokemon = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+export const DivStats = styled.div`
+  width: 100%;
+  display: flex;
+flex-flow: row wrap;
+  line-break: strict;
+  align-items: center;
+  margin-left: 30px;
+`;
+export const DivTitle = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
 `
 
+export const StatusBar = styled.div`
+  width: 100%;
+  height: 6px;
+  background-color: #fcfcfc;
+  border-radius: 3px;
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: ${({ percentual }) => percentual}%;
+    max-width: 120%;
+    height: 6px;
+    ${({ percentual }) =>
+      percentual < 50
+        ? "background-color: #ff0000;"
+        : "background-color: #23dd7a;"}
+    border-radius: 3px;
+  }
+`;
